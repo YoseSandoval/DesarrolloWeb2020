f96c50e96e8f23c5f6591568abb9a736ac7e0849
YoseSandoval <yjsandoval55gmail.com>
2020-09-12 19:06:37 -0600

    Create urls.py

---
+++ HT-5.2/Test/Apps/home/urls.py
@@ -0,0 +1,29 @@
+"""HOME URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import HomeView
+from .views import EstudiantesView
+from .views import AdministradoresView
+from .views import CreditosView
+
+app_name='home'
+urlpatterns = [
+    path('index/', HomeView.as_view(), name='home'),
+    path('estudiantes/', EstudiantesView.as_view(), name='estudiantes'),
+    path('administradores/', AdministradoresView.as_view(), name='administradores'),
+    path('creditos/', CreditosView.as_view(), name='creditos'),
+]
